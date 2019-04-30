c427230500824d0ecdc016efe90949d5ba068cc4
Gitte Kremling <g.kremling@fz-juelich.de>
2019-04-30 12:45:51 +0200

    project mlsdc_convergence initially added

---
+++ pySDC/projects/mlsdc_convergence/lsg_vanderpol.py
@@ -0,0 +1,111 @@
+from pySDC.implementations.collocation_classes.gauss_radau_right import CollGaussRadau_Right
+from pySDC.implementations.problem_classes.Van_der_Pol_implicit import vanderpol
+from pySDC.implementations.sweeper_classes.generic_implicit import generic_implicit
+
+from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
+
+import pickle
+from math import log
+import numpy as np
+
+def main():
+    # initialize level parameters
+    level_params = dict()
+    level_params['restol'] = 1e-14
+
+    # initialize sweeper parameters
+    sweeper_params = dict()
+    sweeper_params['collocation_class'] = CollGaussRadau_Right # Q
+    sweeper_params['num_nodes'] = 7 # M
+    sweeper_params['QI'] = 'IE' # Q_delta
+
+    # initialize problem parameters
+    problem_params = dict()
+    problem_params['newton_tol'] = 1E-12
+    problem_params['newton_maxiter'] = 50
+    problem_params['mu'] = 5
+    problem_params['u0'] = (1.0, 0)
+    problem_params['nvars'] = 2
+
+    # initialize step parameters
+    step_params = dict()
+    step_params['maxiter'] = 100
+
+    # initialize controller parameters
+    controller_params = dict()
+    controller_params['logger_level'] = 30
+
+    # Fill description dictionary for easy hierarchy creation
+    description = dict()
+    description['problem_class'] = vanderpol
+    description['problem_params'] = problem_params
+    description['sweeper_class'] = generic_implicit
+    description['sweeper_params'] = sweeper_params
+    description['step_params'] = step_params
+
+    # set time parameters
+    t0 = 0.
+    Tend = 1.0
+        
+    ### UM NUR UEND (BEI TEND=1) ZU VERGLEICHEN    
+#    level_params['dt'] = 1e-3
+#    description['level_params'] = level_params
+#    
+#    # instantiate the controller
+#    controller = allinclusive_multigrid_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+#    
+#    # get initial values
+#    P = controller.MS[0].levels[0].prob
+#    uinit = P.u_exact(t0)
+#
+#    # call main function to get things done...
+#    uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
+#    
+#    lsg = uend
+    
+    ### UM GANZEN VEKTOR ZU VERGLEICHEN
+    lsg = {}
+    
+    nsteps_arr = [2**i for i in range(3,8)]
+    
+    #verschiedene dt
+    for i,nsteps in enumerate(nsteps_arr):
+        print("nsteps:{}".format(nsteps))
+        
+        # set time step (delta t)
+        dt = 1./nsteps
+        level_params['dt'] = dt
+        description['level_params'] = level_params
+        
+        Tend = t0+dt
+    
+        # instantiate the controller
+        controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+    
+        # get initial values
+        P = controller.MS[0].levels[0].prob
+        uinit = P.u_exact(t0)
+    
+        # call main function to get things done...
+        uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
+        
+        # lsg speichern
+        L = controller.MS[0].levels[0]
+        nodes = [0]
+        nodes.extend(L.sweep.coll.nodes)
+        
+        lsg[nsteps] = [[],[]]
+        for j, node in enumerate(nodes):
+            lsg[nsteps][0].append(L.time+node*L.dt)
+            lsg[nsteps][1].append(L.u[j].values)
+            print('t={}\tu={}'.format(L.time+node*L.dt, L.u[j].values))
+    
+    print(lsg.values)
+            
+    fout = open("data/lsg_vanderpol.pickle", "wb")
+    pickle.dump(lsg, fout)
+    fout.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ pySDC/projects/mlsdc_convergence/plot_errors.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 10:52:21 2018
+
+@author: kremling
+"""
+
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams.update({'font.size': 14})
+
+def plot_errors(fname_errors=["errors.pickle"], figname=None, order_sdc=lambda n: n, order_mlsdc= lambda n: 2*n):
+    # Daten einlesen
+    fin = open(fname_errors, "rb")
+    error = pickle.load(fin)
+    fin.close()
+    
+    # Plotgroesse einstellen
+#    plt.rcParams["figure.figsize"] = [7.5,5.]
+    
+    # Farben und Symbole fuer Linien und Punkte einstellen
+    color = ['orange', 'red', 'magenta', 'blue', 'green']
+    marker = ['x','d','o','^','s']
+    
+    # Grenzen der y-Achse (letzter Punkt der untersten Linie bei MLSDC, erster Fehler bei SDC)
+#    ymin = error[1][(error[1]['niter_arr'][-1],error[1]['nsteps_arr'][0])]/((error[1]['nsteps_arr'][-1]/error[1]['nsteps_arr'][0])**(error[1]['niter_arr'][-1]+1))
+#    ymax = error[0][(error[0]['niter_arr'][0], error[0]['nsteps_arr'][0])]
+        
+    f, axes = plt.subplots(1, 2, sharex=False, sharey=True, figsize=(15,5))
+    
+    # Plot erstellen
+    for i, err in enumerate(error):            
+        dt_arr = 1./np.array(err['nsteps_arr'])
+        
+        axes[i].set_title("{}".format(err['type']))
+        
+        for j, niter in enumerate(err['niter_arr']):
+
+            # erreichte Punkte
+            axes[i].plot(dt_arr, [err[(niter,n)] for n in err['nsteps_arr']], color=color[j], marker=marker[j], linestyle='None', label='k={}'.format(niter))
+            
+            # erwartete Linie: err
+            if err['type'] == 'SDC':
+                axes[i].plot(dt_arr, [(err[(niter,err['nsteps_arr'][0])]/((nstep/err['nsteps_arr'][0])**(order_sdc(niter)))) for nstep in err['nsteps_arr']], color=color[j])
+            elif err['type'] == 'MLSDC':
+                axes[i].plot(dt_arr, [(err[(niter,err['nsteps_arr'][0])]/((nstep/err['nsteps_arr'][0])**(order_mlsdc(niter)))) for nstep in err['nsteps_arr']], color=color[j])
+            
+        axes[i].set_xscale('log', basex=2)
+        axes[i].set_yscale('log')
+        axes[i].set_xlabel(r'$\Delta$t')
+        axes[i].set_ylabel('error')
+        axes[i].set_xlim(dt_arr[0]+dt_arr[0]/8, dt_arr[-1]-dt_arr[-1]/8)
+#        plt.ylim(ymin*1E-1, ymax*1E2)
+#        if err['type'] == 'MLSDC':
+#            plt.legend(title='lines = k')
+#        else:
+        axes[i].legend()
+    
+    plt.show()  
+    
+    if figname:
+        plt.savefig(figname)
+#        plt.savefig('/home10/kremling/Documents/Studium/Gitte/Master/Seminararbeit/seminararbeit/daten/graphics/{}_{}_errors.png'.format(err['type'], dgl))
+        print('figure saved in: {}'.format(figname)) 
+    
+if __name__ == "__main__":
+    plot_errors(order_sdc=lambda n: n, oder_mlsdc=lambda n: 2*n)
