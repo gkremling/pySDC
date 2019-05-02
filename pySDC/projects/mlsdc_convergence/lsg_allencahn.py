d8bbdcbfbe9d86d661afe4e95ff1531923bb1dd5
Gitte Kremling <g.kremling@fz-juelich.de>
2019-05-02 14:42:42 +0200

    implemented tests for allen cahn (just played, not ready yet)

---
+++ pySDC/projects/mlsdc_convergence/lsg_allencahn.py
@@ -0,0 +1,198 @@
+import os
+
+import numpy as np
+import pickle
+
+from pySDC.helpers.stats_helper import filter_stats, sort_stats
+from pySDC.implementations.collocation_classes.gauss_radau_right import CollGaussRadau_Right
+from pySDC.implementations.controller_classes.controller_nonMPI import controller_nonMPI
+from pySDC.implementations.problem_classes.AllenCahn_2D_FD import allencahn_fullyimplicit
+from pySDC.implementations.problem_classes.AllenCahn_2D_FFT import allencahn2d_imex
+from pySDC.implementations.sweeper_classes.generic_implicit import generic_implicit
+from pySDC.implementations.sweeper_classes.imex_1st_order import imex_1st_order
+from pySDC.playgrounds.Allen_Cahn.AllenCahn_monitor import monitor
+
+
+# http://www.personal.psu.edu/qud2/Res/Pre/dz09sisc.pdf
+
+
+def setup_parameters():
+    """
+    Helper routine to fill in all relevant parameters
+
+    Note that this file will be used for all versions of SDC, containing more than necessary for each individual run
+
+    Returns:
+        description (dict)
+        controller_params (dict)
+    """
+
+    # initialize level parameters
+    level_params = dict()
+    level_params['restol'] = 1E-11
+    level_params['dt'] = 1E-05
+    level_params['nsweeps'] = [1]
+
+    # initialize sweeper parameters
+    sweeper_params = dict()
+    sweeper_params['collocation_class'] = CollGaussRadau_Right
+    sweeper_params['num_nodes'] = [5]
+    sweeper_params['QI'] = ['LU']
+    sweeper_params['spread'] = False
+
+    # This comes as read-in for the problem class
+    problem_params = dict()
+    problem_params['nu'] = 2
+    problem_params['nvars'] = [(128, 128)]
+    problem_params['eps'] = [0.04]
+    problem_params['newton_maxiter'] = 100
+    problem_params['newton_tol'] = 1E-12
+    problem_params['lin_tol'] = 1E-12
+    problem_params['lin_maxiter'] = 100
+    problem_params['radius'] = 0.25
+
+    # initialize step parameters
+    step_params = dict()
+    step_params['maxiter'] = 50
+
+    # initialize controller parameters
+    controller_params = dict()
+    controller_params['logger_level'] = 20
+    # controller_params['hook_class'] = monitor
+
+    # fill description dictionary for easy step instantiation
+    description = dict()
+    description['problem_class'] = allencahn_fullyimplicit  # pass problem class
+    description['problem_params'] = problem_params  # pass problem parameters
+    description['sweeper_class'] = generic_implicit  # pass sweeper (see part B)
+    description['sweeper_params'] = sweeper_params  # pass sweeper parameters
+    description['level_params'] = level_params  # pass level parameters
+    description['step_params'] = step_params  # pass step parameters
+
+    return description, controller_params
+
+
+def setup_parameters_FFT():
+    """
+    Helper routine to fill in all relevant parameters
+
+    Note that this file will be used for all versions of SDC, containing more than necessary for each individual run
+
+    Returns:
+        description (dict)
+        controller_params (dict)
+    """
+
+    # initialize level parameters
+    level_params = dict()
+    level_params['restol'] = 1E-11
+    level_params['dt'] = 1E-04
+    level_params['nsweeps'] = [1]
+
+    # initialize sweeper parameters
+    sweeper_params = dict()
+    sweeper_params['collocation_class'] = CollGaussRadau_Right
+    sweeper_params['num_nodes'] = [5]
+    sweeper_params['QI'] = ['LU']
+    sweeper_params['spread'] = False
+
+    # This comes as read-in for the problem class
+    problem_params = dict()
+    problem_params['nu'] = 2
+    problem_params['nvars'] = [(128, 128)]
+    problem_params['eps'] = [0.04]
+    problem_params['newton_maxiter'] = 100
+    problem_params['newton_tol'] = 1E-12
+    problem_params['lin_tol'] = 1E-12
+    problem_params['lin_maxiter'] = 100
+    problem_params['radius'] = 0.25
+
+    # initialize step parameters
+    step_params = dict()
+    step_params['maxiter'] = 50
+
+    # initialize controller parameters
+    controller_params = dict()
+    controller_params['logger_level'] = 30
+#    controller_params['hook_class'] = monitor
+
+    # fill description dictionary for easy step instantiation
+    description = dict()
+    description['problem_class'] = allencahn2d_imex  # pass problem class
+    description['problem_params'] = problem_params  # pass problem parameters
+    description['sweeper_class'] = imex_1st_order  # pass sweeper (see part B)
+    description['sweeper_params'] = sweeper_params  # pass sweeper parameters
+    description['level_params'] = level_params  # pass level parameters
+    description['step_params'] = step_params  # pass step parameters
+
+    return description, controller_params
+
+
+def run_reference(nsteps_arr):
+    """
+    Routine to run particular SDC variant
+
+    Args:
+        Tend (float): end time for dumping
+    """
+
+    # load (incomplete) default parameters
+    description, controller_params = setup_parameters_FFT()
+
+    # setup parameters "in time"
+    t0 = 0.
+    
+    lsg = {}
+    
+    #verschiedene dt
+    for i,nsteps in enumerate(nsteps_arr):
+        print("nsteps:{}".format(nsteps))
+        
+        # set time step (delta t)
+        dt = 1./nsteps
+        description['level_params']['dt'] = dt
+        
+        Tend = t0+dt
+    
+        # instantiate the controller
+        controller = controller_nonMPI(num_procs=1, controller_params=controller_params, description=description)
+    
+        # get initial values
+        P = controller.MS[0].levels[0].prob
+        uinit = P.u_exact(t0)
+    
+        # call main function to get things done...
+        uend, stats = controller.run(u0=uinit, t0=t0, Tend=Tend)
+        
+        # lsg speichern
+        L = controller.MS[0].levels[0]
+        nodes = [0]
+        nodes.extend(L.sweep.coll.nodes)
+        
+        lsg[nsteps] = [[],[]]
+        for j, node in enumerate(nodes):
+            lsg[nsteps][0].append(L.time+node*L.dt)
+            lsg[nsteps][1].append(L.u[j].values)
+            print('t={}\tu={}'.format(L.time+node*L.dt, L.u[j].values))
+    
+    print(lsg.values)
+            
+    fout = open("data/lsg_allencahn.pickle", "wb")
+    pickle.dump(lsg, fout)
+    fout.close()
+
+
+def main(cwd=''):
+    """
+    Main driver
+
+    Args:
+        cwd (str): current working directory (need this for testing)
+    """
+    nsteps_arr = [2**i for i in range(10,20)]
+    run_reference(nsteps_arr)
+
+
+
+if __name__ == "__main__":
+    main()
